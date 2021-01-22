1e1af7c485980dcf25b84f900f63221bd811e3ac
Cosmin <cosmin@webdinasty.ro>
2021-01-22 12:55:12 +0200

    added test.sql

---
+++ test.sql
@@ -0,0 +1,8 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `Cars` (`id`, `brand`, `model`, `color`) VALUES
+(1,	'VW',	'Golf',	'Red'),
+(2,	'Toyota',	'Celica',	'Black'),
+(3,	'Ford',	'Focus',	'IDGAF :) '),
+(4,	'Dacia',	'Logan',	'IDK :('),
+(5,	'Mazda',	'RX 7',	'Orange');
